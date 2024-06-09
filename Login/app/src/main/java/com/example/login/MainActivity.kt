c6cf940a30458b14b07c43fa6174368363a84761
Jinto <jinto.v_r@daimler.com>
2024-06-09 13:04:08 +0530

    Login 1

--- Login/app/src/main/java/com/example/login/MainActivity.kt
+++ Login/app/src/main/java/com/example/login/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.login
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +19,25 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //binding.button.setOnKeyListener( );
+
+
+       // View.OnKeyListener l = ;
+        binding.button.setOnKeyListener { view: View, i: Int, keyEvent: KeyEvent ->
+            val email = binding.editTextTextEmailAddress.text.toString();
+            val password = binding.editTextNumberPassword.text.toString();
+
+           Log.i("MainActivity", "OnCreate: email $email , password : $password i = $i" , );
+           return@setOnKeyListener true;
+        }
+
+
+        binding.button.setOnClickListener {
+            val email = binding.editTextTextEmailAddress.text.toString();
+            val password = binding.editTextNumberPassword.text.toString();
+            Log.i("MainActivity", "OnCreate: email $email , password : $password" , );
+        }
     }
 }
+
