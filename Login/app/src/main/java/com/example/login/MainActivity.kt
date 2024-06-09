6c78f66c2ee34d105b636d4aac08a454fa208574
Jinto <jinto.v_r@daimler.com>
2024-06-09 16:15:15 +0530

    ToastMesageSnackbar

--- Login/app/src/main/java/com/example/login/MainActivity.kt
+++ Login/app/src/main/java/com/example/login/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.login.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,19 +27,31 @@ class MainActivity : AppCompatActivity() {
 
        // View.OnKeyListener l = ;
         binding.button.setOnKeyListener { view: View, i: Int, keyEvent: KeyEvent ->
-            val email = binding.editTextTextEmailAddress.text.toString();
-            val password = binding.editTextNumberPassword.text.toString();
 
-           Log.i("MainActivity", "OnCreate: email $email , password : $password i = $i" , );
            return@setOnKeyListener true;
         }
 
 
         binding.button.setOnClickListener {
-            val email = binding.editTextTextEmailAddress.text.toString();
-            val password = binding.editTextNumberPassword.text.toString();
+
+            var email  = binding.emailAddress.text.toString();
+            var password  = binding.password.text.toString();
             Log.i("MainActivity", "OnCreate: email $email , password : $password" , );
+
+            var message = getString(R.string.messageText,email,password);
+
+            showMessage(message)
+
+            Snackbar.make(it, message , Snackbar.LENGTH_INDEFINITE)
+                .setAction( "moreInfo") {showMessage(message)}
+                .show ();
+
+
         }
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
 
